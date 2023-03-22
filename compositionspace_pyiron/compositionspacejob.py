b9bef1ba0fcb86de08f442b0db772b054c1af126
Sarath Menon <sarathmenon@mailbox.org>
2023-03-22 14:44:08 +0100

    update compositionspace job

--- compositionspace_pyiron/compositionspacejob.py
+++ compositionspace_pyiron/compositionspacejob.py
@@ -26,9 +26,17 @@ class CompositionSpace(GenericJob):
         self.input.fileindex=0
         self.input.cluster_id=0
         self.input.plot=False
-        self.input.plot3d=False     
+        self.input.plot3d=False
         self._input_dict = {}
-
+        self._analyse_PCA_cumsum = False
+        self._analyse_bics_minimization= False
+        self._analyse_composition_clustering = False
+        self._analyse_dbscan_clustering = False
+        self._analyse_PCA_cumsum_done = False
+        self._analyse_bics_minimization_done = False
+        self._analyse_composition_clustering_done = False
+        self._analyse_dbscan_clustering_done = False
+        
     @property
     def _default_input(self):
         return {
@@ -37,11 +45,11 @@ class CompositionSpace(GenericJob):
             "n_big_slices": 10,
             "voxel_size": 2,
             "bics_clusters": 10,
-            "n_phases": 3,
+            "n_phases": 2,
             "ml_models": {
                 "name": "GaussianMixture", 
                 "GaussianMixture": {
-                    "n_components": 3,
+                    "n_components": 2,
                     "max_iter": 100000,
                     "verbose": 0,
                     },
@@ -50,8 +58,8 @@ class CompositionSpace(GenericJob):
                     "n_estimators": 0,
                     },
                 "DBScan": {
-                    "eps": 5,
-                    "min_samples": 50,
+                    "eps": 3,
+                    "min_samples": 5,
                     },
             }
         }
@@ -72,7 +80,12 @@ class CompositionSpace(GenericJob):
         indict = self._create_input(self._default_input, self.input)
         indict["output_path"] = self.working_directory
         self._input_dict = indict
-
+        self.input.fileindex=0
+        self.input.plot=True
+        self.input.plot3d=True
+        if self.input.input_path is None:
+            raise FileNotFoundError("Input path needs to be set before analysis")
+            
     def _prepare_comp(self):
         if self._comp is None:      
             self._comp = CompositionClustering(self._input_dict)
@@ -97,8 +110,7 @@ class CompositionSpace(GenericJob):
             self._data.voxel_files[self.input.fileindex])
         self._composition_clusters_found = True
 
-    def _get_dbscan_clustering(self):
-        
+    def _get_dbscan_clustering(self):        
         if not self._composition_clusters_found:
             self._get_composition_clusters()
         
@@ -112,40 +124,41 @@ class CompositionSpace(GenericJob):
         if self._comp is None:
             raise RuntimeError("Run any composition calculation before to plot")
         return self._comp.plot3d(**kwargs)
+    
+    def analyse_PCA_cumsum(self):
+        self._analyse_PCA_cumsum = True
 
-    def analyse(self, analysis=["PCA_cumsum", "bics_minimization",
-        "composition_clustering", "dbscan_clustering"], 
-        fileindex=0, cluster_id=0,
-        plot=False, plot3d=False):
-        
-        if self.input.input_path is None:
-            raise FileNotFoundError("Input path needs to be set before analysis")
-            
-        self.input.analysis = [x.lower() for x in analysis]
-        self.input.fileindex=fileindex
-        self.input.fileindex=fileindex
-        self.input.plot=plot
-        self.input.plot3d=plot3d            
+    def analyse_bics_minimization(self):
+        self._analyse_bics_minimization= True
 
-    def run_static(self):
+    def analyse_composition_clustering(self):
+        self._analyse_composition_clustering = True
         
+    def analyse_dbscan_clustering(self):
+        self._analyse_dbscan_clustering = True        
+
+    def run_static(self):   
         self.status.running = True
         self._data = DataPreparation(self._input_dict)
-        self._data.chunkify_apt_df()
+        self._data.get_big_slices()
         self._data.get_voxels()
         self._data.calculate_voxel_composition()
-
-        if "pca_cumsum" in self.input.analysis:
+        
+        if self._analyse_PCA_cumsum:
             self._get_PCA_cumsum()
+            self._analyse_PCA_cumsum_done = True
 
-        if "bics_minimization" in self.input.analysis:
+        if self._analyse_bics_minimization:
             self._get_bics_minimization()
+            self._analyse_bics_minimization_done = True
 
-        if "composition_clustering" in self.input.analysis:
+        if self._analyse_composition_clustering:
             self._get_composition_clusters()
+            self._analyse_composition_clustering_done = True
 
-        if "dbscan_clustering" in self.input.analysis:
+        if self._analyse_dbscan_clustering:
             self._get_dbscan_clustering()
+            self._analyse_dbscan_clustering_done = True
 
         self.status.collect = True
 
